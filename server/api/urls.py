930ef9d786c98d33f77fa03897ad7b7175238129
MateuszSu <259160@student.pwr.edu.pl>
2023-05-05 15:32:46 +0200

    cached lecturers and added settings json

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('ttbyname', views.getTimeTableByName, name='ttbyname'),
     path('classrooms', views.getAllClassroms, name='allClassrooms'),
+    path('lecturers', views.getAllLecturers, name='allLecturers'),
 ]
