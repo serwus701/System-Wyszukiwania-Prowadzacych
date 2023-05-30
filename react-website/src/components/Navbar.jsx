a5cd77f0ab89b089582eb95292717ff5d5eb318f
Michał Malewicz <serwus701@gmail.com>
2023-05-30 23:42:42 +0200

    add search record interface

--- react-website/src/components/Navbar.jsx
+++ react-website/src/components/Navbar.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "../styles/Nav.css";
+import "../styles/Navbar.css";
 // value -> link, name -> wyświetlana nazwa
 class BarElement extends React.Component {
-  render(){
+  render() {
     return (
       <Nav.Link href={this.props.value}><h1>{this.props.name}</h1></Nav.Link>
     );
   }
 }
 const Bar = () => {
-    return (
-      <Navbar>
-        <Container classname="Navbar">
-            <Nav className="links">
-              <img src='/resources/logo.png' alt="logo"></img>
-              <BarElement value="profile" name='Profil' ></BarElement>
-              <BarElement value="consultations" name='Konsultacje'></BarElement>
-              <BarElement value="reservations" name='Rezerwacje'></BarElement>
-              <BarElement value="administration" name='Administracja'></BarElement>
-              <BarElement value="result" name='Szukaj'></BarElement>
-              <BarElement value="logout" name='Wyloguj'></BarElement>
-            </Nav>
-        </Container>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar>
+      <Container classname="Navbar">
+        <Nav className="links">
+          <img src='/resources/logo.png' alt="logo"></img>
+          <BarElement value="profile" name='Profil' ></BarElement>
+          <BarElement value="consultations" name='Konsultacje'></BarElement>
+          <BarElement value="reservations" name='Rezerwacje'></BarElement>
+          <BarElement value="administration" name='Administracja'></BarElement>
+          <BarElement value="result" name='Szukaj'></BarElement>
+          <BarElement value="logout" name='Wyloguj'></BarElement>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Bar;
--- react-website/src/pages/result.jsx
+++ react-website/src/pages/result.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
- 
+import InformationList from '../components/InformationList.jsx';
+import Calendar from '../components/Calendar.jsx';
+
+import '../styles/Result.css';
+
 const Result = () => {
+    const listItems = {
+        "dane o kursie": {
+            'Godziny zajęć': '8 - 16',
+            'Nazwa przedmiotu': "Programowanie w języku C",
+            'sala': 'C-3:229'
+        },
+        "dane o prowadzącym": {
+            "Imię i nazwisko": "Jan Kowalski",
+            "katedra": "Katedra Informatyki"
+        },
+        "Ogloszenie": {
+            "Ogloszenie": "Treść ogłoszenia",
+        },
+        "Konsultacje": {
+            "zoom": "xxx",
+            "teams": "xxx",
+            "web": "xxx"
+        }
+    };
+
     return (
-        <div>
-            <h1>WIP Result</h1>
+        <div className='result-box'>
+            <div>
+                <InformationList listItems={listItems} />
+            </div>
+            <div>
+                <Calendar />
+            </div>
         </div>
     );
 };
- 
+
 export default Result;
