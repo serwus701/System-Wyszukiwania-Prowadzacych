b1a7e7b5c7a3f996eb5c8e05ed27afeae74af5e7
Michał Malewicz <serwus701@gmail.com>
2023-06-05 03:24:19 +0200

    add records to calendar (not woprking yet xD)

--- react-website/src/components/Calendar/Board.jsx
+++ react-website/src/components/Calendar/Board.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import './Board.css';
 
-function Board() {
+function Board(props) {
+
+    function addRecord(event) {
+        let divTop = 6.59 * (event.timeStart - 7);
+        let divLeft = 0;
+        let height = 6.59 * (event.timeEnd - event.timeStart);
+
+        let newDiv = document.createElement('div');
+        newDiv.style.position = 'absolute';
+        newDiv.style.top = `${divTop}%`;
+        newDiv.style.left = `${divLeft}px`;
+        newDiv.style.width = '20%';
+        newDiv.style.height = `${height}%`;
+        newDiv.style.backgroundColor = "#2F589D";
+        newDiv.style.borderRadius = '15px';
+
+        let targetDiv = document.querySelector('#grid-container');
+        targetDiv.appendChild(newDiv);
+    }
+
     return (
-        <div className="grid-container">
+        <div className="grid-container" id="grid-container">
             {Array(15)
                 .fill(0)
                 .map((row, rowIndex) => (
@@ -15,6 +34,13 @@ function Board() {
                             ))}
                     </div>
                 ))}
+            {
+                props.events.map((event) => (
+                    // console.log(event)
+                    addRecord(event)
+                ))
+            }
+
         </div>
     );
 }
